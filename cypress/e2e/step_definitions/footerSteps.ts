aa5a24354d6d43283c995e288cd5f5eba0a64240
Anna Ruban <anna.ruban0902@gmail.com>
2025-03-26 12:42:27 +0200

    TC01-TC03 Implementation

--- cypress/e2e/step_definitions/footerSteps.ts
+++ cypress/e2e/step_definitions/footerSteps.ts
@@ -1,23 +1,27 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import HomePage from "../../pages/homePage";
-import { footerCompanyItems } from "../../utils/constants";
+import { footerBlocksMap } from "../../utils/constants";
 
 const homePage = new HomePage();
 
-Given("I open the {string} page", (page: string) => {
-    homePage.navigateTo(page);
-});
-
-When('I check that the {string} block links are visible in the footer', () => {
-    footerCompanyItems.forEach(({ footerName }) => {
-        homePage.getFooterItemElement(footerName).should('be.visible');
-      });
+When('I check that the {string} block links are visible in the footer', (blockName: string) => {
+  const items = footerBlocksMap[blockName];
+  if (!items) {
+    throw new Error(`No footer block found for: ${blockName}`);
+  }
+  items.forEach(({ footerName }) => {
+    homePage.getFooterItemElement(footerName).should('be.visible');
+  });
 });
 
 Then("each link in the {string} block should navigate to its corresponding page", (blockName: string) => {
-    footerCompanyItems.forEach(({ footerName, itemUrl }) => {
-      homePage.clickFooterItem(footerName);
-      homePage.getCurrentUrl().should("include", itemUrl);
-      cy.visit("/");
-    });
+  const items = footerBlocksMap[blockName];
+  if (!items) {
+    throw new Error(`No footer block found for: ${blockName}`);
+  }
+  items.forEach(({ footerName, itemUrl }) => {
+    homePage.clickFooterItem(footerName);
+    homePage.getCurrentUrl().should("include", itemUrl);
+    cy.visit("/");
   });
+});
---
+++ cypress/e2e/step_definitions/formSteps.ts
@@ -0,0 +1,20 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import HomePage from "../../pages/homePage";
+import { generateValidEmail, generateInvalidEmail } from "cypress/utils/dataGenerator";
+
+const homePage = new HomePage();
+
+When('I enter a valid email', () => {
+    homePage.fillContactWithUsForm(generateValidEmail());
+});
+
+When('I enter an invalid email', () => {
+    homePage.fillContactWithUsForm(generateInvalidEmail());
+});
+
+Then('I see an error message', () => {
+    homePage.getConnectWithUsInputElement().then(($input) => {
+        const validationMessage = ($input[0] as HTMLInputElement).validationMessage;
+        expect(validationMessage).to.not.be.empty;
+        })
+})
